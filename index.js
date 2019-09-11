547da5807ce1077bd9a34556177124005b0b8467
D Y <yomtovdaniel1@gmail.com>
2019-09-11 11:59:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 let katzDeliLine = [];
 
-function takeANumber(lineOfPeople, newPersonsName) {
-  let personsPlace = katzDeliLine.indexOf(newPersonsName) + 1;
+function takeANumber(currentLineOfPeople, newPersonsName) {
+  currentLineOfPeople.push(newPersonsName);
+  let theName = (currentLineOfPeople.indexOf(newPersonsName) + 1);
+  return `Welcome, ${newPersonsName}. You are number ${theName} in line.`;
+}
 
-  return `Welcome, ${newPersonsName}. You are number ${personsPlace} in line.`
+function nowServing(katzDeliLine) {
+  if(katzDeliLine[0] === undefined) {
+    return "There is nobody waiting to be served!"
+  } else {
+    let personInLine = katzDeliLine.shift();
+    return `Currently serving ${personInLine}.`;
+  }
+}
 
+function currentLine(currentLineOfPeople) {
+  let newArr = [];
+  for(let i = 0; i < currentLineOfPeople.length; i++) {
+    if(currentLineOfPeople[i] === undefined){
+      return "The line is currently empty.";
+    } else {
+      newArr.push(`${i + 1}. ${currentLineOfPeople[i]}`);
+    }
+  }
+  return (`The line is currently: ${newArr}`);
 }
