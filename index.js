e89d2f649b3bd57e13d56aa6f3e092388ff33ed0
D Y <yomtovdaniel1@gmail.com>
2019-09-11 12:11:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(currentLineOfPeople) {
   let newArr = [];
-  newArr.splice(0, 0, ' ')
+  newArr.splice(0, 0, ' ');
   for(let i = 0; i < currentLineOfPeople.length; i++) {
     if(currentLineOfPeople[i] === undefined){
       return "The line is currently empty.";
